934117f99785c7c881f7d9a960c32638b1a442de
olena-shpak <149821095+olena-shpak@users.noreply.github.com>
2023-12-18 13:22:12 +0200

    Add files via upload

--- homework005.js
+++ homework005.js
@@ -18,10 +18,13 @@ arr1[3] ="friend";
 arr1[2] = "cola";
 console.log(arr1)
 
-const multiplicationTable = Array.from({ length: 5 }, (_, rowIndex) =>
-  Array.from({ length: 5 }, (_, colIndex) => (rowIndex ) * (colIndex ))
-);
+const multiplicationTable = [[0, 0, 0, 0, 0],
+                             [0, 1, 2, 3, 4],
+                             [0, 2, 4, 6, 8],
+                             [0, 3, 6, 9, 12],
+                             [0, 4, 8, 12, 16]]
 console.log(multiplicationTable);
+
 
 const tableWithoutZeros = multiplicationTable.map(row =>
   row.slice().filter(element => element !== 0))
